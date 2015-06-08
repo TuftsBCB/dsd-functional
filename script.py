e467af06c9604ff6fc362aeb8bf11c129a0bd4ba
Tim Lim <timlim99@hotmail.com>
2015-06-08 17:55:16 -0400

    added GO to dict function, plus code to construct overlap matrix

--- script.py
+++ script.py
@@ -70,4 +70,62 @@ HEmatrix = dsd.hematrix(np.array(nx.adjacency_matrix(G,nodeList).todense()))
 # construct DSD
 D = dsd.DSD(HEmatrix,LMset)
 
+# get list of nodes in LMSet
+LMnodes = [nodeList[index] for index in LMset]
 
+def GOToDict(LMnodes, GOfile):
+    """
+    given set of nodes and name of GO file,
+    return dictionary with node(gene id) as key, list of labels as value
+    """
+    node_labels = {}
+
+    with open(GOfile) as f:
+        for line in f:
+            # assume first key in each line is either gene id or empty (a tab char)
+            # only consider lines with gene ids
+            if line[0] == '\t':
+                continue
+            part = line.strip().split('\t', 1)
+
+            # if gene id in our landmark set, add labels to dict
+            # if no label, add empty list
+            if part[0] not in LMnodes:
+                continue
+            if len(part) == 2:
+                labels = part[1].split('; ')
+            else:
+                labels = []
+            node_labels[part[0]] = labels
+
+    # if node not found in GO file, consider it as having no labels
+    for node in LMnodes:
+        if node not in node_labels:
+            node_labels[node] = []
+
+    return node_labels
+
+# assuming GOfile is in same directory as ppi file, replace .ppi extension with NCBI_to_GO
+GOfile = options.infile[:-4]
+GOfile += "_NCBI_to_GO"
+node_labels = GOToDict(LMnodes, GOfile)
+
+#for node in node_labels:
+#    print "{}: ".format(node), node_labels[node]
+
+# construct K = overlap matrix
+num_nodes = len(node_labels)
+K = np.zeros((num_nodes, num_nodes), dtype=int)
+
+# if there is function overlap, set K[ij] and K[ji] to 1
+for i in xrange(1,num_nodes):
+    nd = LMnodes[i]
+    for j in xrange(i+1,num_nodes):
+        for label in node_labels[nd]:
+            if label in node_labels[LMnodes[j]]:
+                K[i][j] = 1
+                K[j][i] = 1
+                break
+
+for i in range(num_nodes):
+    print K[i]
